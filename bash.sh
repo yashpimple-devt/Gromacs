c10be8b474eb2452eb513eceaa5e21692a1257c1
yashpimple-devt <146062073+yashpimple-devt@users.noreply.github.com>
2023-10-05 11:55:31 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,6 +2,7 @@
 
 # Install additional dependencies
 sudo apt-get install -y gcc g++ make
+sudo apt install nvidia-cuda-toolkit
 
 # Download GROMACS 19.04 source code
 wget ftp://ftp.gromacs.org/pub/gromacs/gromacs-2019.4.tar.gz
@@ -13,10 +14,19 @@ cd gromacs-2019.4
 # Configure and compile GROMACS
 mkdir build
 cd build
-cmake .. -DGMX_BUILD_OWN_FFTW=ON
+cmake .. -DGMX_GPU=ON -DGMX_BUILD_OWN_FFTW=ON
 make
 sudo make install
 
 echo 'export PATH=/home/yashnileshpimple/gromacs-2019.4/build/bin:$PATH'
 
 gmx --version
+
+
+# for GPU setup
+
+sudo apt-get update
+sudo apt-get install -y nvidia-driver-<version>
+
+sudo reboot
+nvidia-smi
